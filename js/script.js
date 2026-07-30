87c6361184fcedc38e9b1a3fc103d534e5cafd81
afokorn-source <afokorn@gmail.com>
2026-07-30 06:38:39 -0400

    Completed Assignment 9

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
-// Prompt the user for their details
-let username = prompt("Enter your name:");
-let age = Number(prompt("Enter your age:"));
-let email = prompt("Enter your email address:");
+// A. Store your name inside a variable and log it to the console.
+let name = "Tabitha";
+console.log(name);
 
-// Check the user's age
-if (age < 12) {
-    alert(
-        "Hi " + username +
-        ", you are " + age +
-        " years old and you are too young to register. Sorry 😔😔"
-    );
-} else if (age >= 12 && age < 18) {
-    alert(
-        "Hi " + username +
-        ", you are " + age +
-        " years old and you have limited options to register for. We will keep in touch via your email: " +
-        email + ". 👏🏽👏🏽"
-    );
-} else {
-    alert(
-        "Hi " + username +
-        ", you are " + age +
-        " years old and you can register for any option of your choosing. We will keep in touch via your email: " +
-        email + ". 👏🏽👏🏽"
-    );
-}
+// B. Store your age inside a variable and log it to the console.
+let age = 25;
+console.log(age);
+
+// C. Add two numbers and store it in a variable and log it to the console.
+let addition = 10 + 5;
+console.log(addition);
+
+// D. Subtract two numbers and store it in a variable and log it to the console.
+let subtraction = 10 - 5;
+console.log(subtraction);
+
+// E. Divide two numbers and store it in a variable and log it to the console.
+let division = 20 / 5;
+console.log(division);
+
+// F. Multiply two numbers and store it in a variable and log it to the console.
+let multiplication = 10 * 5;
+console.log(multiplication);
+
+// G. Find the remainder of 15 % 4 and store it in a variable and log it to the console.
+let remainder = 15 % 4;
+console.log(remainder);
+
+// H. Store a boolean value inside a variable and log it to the console.
+let isStudent = true;
+console.log(isStudent);
+
+// I. Set the value of a variable to a number and log the type of data it is to the console.
+let numberValue = 100;
+console.log(typeof numberValue);
+
+// J. Set the value of a variable to a string and log the type of data it is to the console.
+let stringValue = "Hello World";
+console.log(typeof stringValue);
